e74f2412762b0254ea4792b795c5a9f813938109
leonardonavarro <leonardonavarro2403@gmail.com>
2023-01-30 16:28:31 -0500

    add counterclicks

--- lib/main.dart
+++ lib/main.dart
@@ -19,49 +19,78 @@ class IndexPage extends StatelessWidget {
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                width: screenSize.width * 0.5,
-                height: screenSize.height * 0.1,
-                color: Colors.blue,
-                child: const Center(child: Text('Header')),
-              ),
-              Container(
-                width: screenSize.width * 0.5,
-                color: Colors.green,
-                height: screenSize.height * 0.1,
-                child: const Center(
-                  child: Text('navbar'),
-                ),
-              ),
-            ],
-          ),
-          Container(
-            height: screenSize.height * 0.8,
-            color: Colors.yellow,
-          ),
-          Container(
-              width: screenSize.width,
+      body: Column(children: [
+        Row(
+          children: [
+            ChangeColor(
+              text: 'Header',
+              width: screenSize.width * 0.5,
               height: screenSize.height * 0.1,
-              color: Colors.black,
-              child: const Center(
-                child: Text('footer', style: TextStyle(color: Colors.white)),
-              )),
-        ],
-      ),
+              color: Colors.blue,
+            ),
+            ChangeColor(
+              text: 'Navbar',
+              width: screenSize.width * 0.5,
+              height: screenSize.height * 0.1,
+              color: Colors.green,
+            ),
+          ],
+        ),
+        ChangeColor(
+          text: 'Body',
+          width: screenSize.width,
+          height: screenSize.height * 0.8,
+          color: Colors.yellow,
+        ),
+        ChangeColor(
+          text: 'Footer',
+          width: screenSize.width,
+          height: screenSize.height * 0.1,
+          color: Colors.black,
+        ),
+      ]),
     );
   }
 }
 
-class Changecolor extends StatefulWidget {
-  const Changecolor({super.key});
+class ChangeColor extends StatefulWidget {
+  final double width;
+  final double height;
+  final String text;
+  Color color;
+
+  ChangeColor({
+    super.key,
+    required this.width,
+    required this.height,
+    required this.color,
+    required this.text,
+  });
 
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement  createState
-    throw UnimplementedError();
+  State<StatefulWidget> createState() => ChangeColorState();
+}
+
+class ChangeColorState extends State<ChangeColor> {
+  int count = 0;
+  List<Color> colors = [Colors.blue, Colors.green, Colors.yellow, Colors.black];
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          setState(() {
+            widget.color = colors[count % colors.length];
+            count += 1;
+          });
+        },
+        child: Container(
+          width: widget.width,
+          height: widget.height,
+          color: widget.color,
+          child: Center(
+            child: Text("${widget.text} \nClicks $count"),
+          ),
+        ));
   }
 }
