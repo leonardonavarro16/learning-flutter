f32f0760ad7ff333ac2c002044a0835061b8b603
Christian <christian.garcerant@uac.edu.co>
2023-03-02 15:33:09 -0500

    created drawer

--- lib/widgets/layout.dart
+++ lib/widgets/layout.dart
@@ -10,16 +10,48 @@ class Layout extends StatelessWidget {
     return LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
       return Scaffold(
-          body: Container(
-            color: Colors.black,
-            height: constraints.maxHeight,
-            width: constraints.maxWidth,
-            child: content,
+        body: Container(
+          color: Colors.black,
+          height: constraints.maxHeight,
+          width: constraints.maxWidth,
+          child: content,
+        ),
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(constraints.maxHeight * 0.075),
+          child: const NavBar(),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: const <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text(
+                  'Drawer Header',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.message),
+                title: Text('Messages'),
+              ),
+              ListTile(
+                leading: Icon(Icons.account_circle),
+                title: Text('Profile'),
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('Settings'),
+              ),
+            ],
           ),
-          appBar: PreferredSize(
-            preferredSize: Size.fromHeight(constraints.maxHeight * 0.075),
-            child: const NavBar(),
-          ));
+        ),
+      );
     });
   }
 }
