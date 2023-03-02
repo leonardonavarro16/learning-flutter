8f5254248bdf546ca525067ab108c5d9d6dc0d6d
Christian <christian.garcerant@uac.edu.co>
2023-03-02 16:52:17 -0500

    nav_Bar was re builded, returned AppBar directly in Layout

--- lib/widgets/layout.dart
+++ lib/widgets/layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:swc_front/pages/login_page.dart';
 import 'package:swc_front/widgets/nav_bar.dart';
 
 class Layout extends StatelessWidget {
@@ -21,32 +22,46 @@ class Layout extends StatelessWidget {
           child: const NavBar(),
         ),
         drawer: Drawer(
+          backgroundColor: Colors.black,
           child: ListView(
             padding: EdgeInsets.zero,
             children: const <Widget>[
               DrawerHeader(
                 decoration: BoxDecoration(
-                  color: Colors.blue,
+                  color: Color.fromARGB(216, 243, 89, 89),
                 ),
                 child: Text(
                   'Drawer Header',
                   style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
+                    color: Colors.black,
+                    fontSize: 30,
                   ),
                 ),
               ),
               ListTile(
-                leading: Icon(Icons.message),
-                title: Text('Messages'),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const LoginPage()),
+                    );
+                },
+                title: Text('Listar anuncios'),
+                leading: Icon(Icons.post_add_outlined),
+                iconColor: Colors.red,
+                textColor: Color.fromARGB(215, 255, 255, 255),
               ),
               ListTile(
-                leading: Icon(Icons.account_circle),
-                title: Text('Profile'),
+                title: Text('Crear anuncio'),
+                leading: Icon(Icons.announcement_outlined),
+                iconColor: Colors.red,
+                textColor: Color.fromARGB(215, 255, 255, 255),
               ),
               ListTile(
-                leading: Icon(Icons.settings),
                 title: Text('Settings'),
+                leading: Icon(Icons.settings),
+                iconColor: Colors.red,
+                textColor: Color.fromARGB(215, 255, 255, 255),
               ),
             ],
           ),
