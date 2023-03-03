89548536fc4b2ca3b9eca65a465ff58b74a10601
Christian <christian.garcerant@uac.edu.co>
2023-03-03 13:10:01 -0500

    Edit the AgeFormField widget in order to make it customizable

--- lib/widgets/utils/age_form_field.dart
+++ lib/widgets/utils/age_form_field.dart
@@ -1,32 +1,41 @@
 import 'package:flutter/material.dart';
 
 class AgeFormField extends StatefulWidget {
-  const AgeFormField({super.key});
+  final int minAge;
+  final int maxAge;
+  final String label;
+  const AgeFormField({super.key, this.minAge=18, this.maxAge=65, this.label='Selecciona tu edad:'});
 
   @override
   State<StatefulWidget> createState() => _SliderInputState();
 }
 
 class _SliderInputState extends State<AgeFormField> {
-  double sliderValue = 18;
+  late int sliderValue;
+
+  @override
+  void initState() {
+    sliderValue = widget.minAge;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const Text(
-          'Selecciona tu edad:',
-          style: TextStyle(color: Colors.white),
+        Text(
+          widget.label,
+          style: const TextStyle(color: Colors.white),
         ),
         Slider(
-          value: sliderValue,
-          min: 18,
-          max: 65,
-          divisions: 47,
+          value: sliderValue.toDouble(),
+          min: widget.minAge.toDouble(),
+          max: widget.maxAge.toDouble(),
+          divisions: widget.maxAge - widget.minAge,
           label: sliderValue.toString(),
           onChanged: (double value) {
             setState(() {
-              sliderValue = value;
+              sliderValue = value.toInt();
             });
           },
         )
