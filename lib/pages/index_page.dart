106330db8ac985b7d1432f204a422f96fe0ec832
Christian <christian.garcerant@uac.edu.co>
2023-03-13 16:56:42 -0500

    temp2

--- lib/pages/index_page.dart
+++ lib/pages/index_page.dart
@@ -1,8 +1,11 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:swc_front/widgets/advert_list.dart';
 import 'package:swc_front/widgets/layout.dart';
 import 'package:swc_front/models/advert.dart';
+import 'package:http/http.dart' as http;
 
 import '../states/adverts.dart';
 
@@ -11,11 +14,47 @@ class IndexPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Advert> adverts = context.watch<AdvertsState>().adverts;
     return Layout(
-      content: ListView(
-        children: [const SizedBox(height: 20), AdverList(adverts: adverts)],
+      content: FutureBuilder<List<Advert>>(
+        future: fetchAdverts(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List<Advert> adverts = snapshot.data as List<Advert>;
+            return ListView(
+              children: [
+                const SizedBox(height: 20),
+                AdverList(adverts: adverts)
+              ],
+            );
+          } else if (snapshot.hasError) {
+            print('snapshot.error');
+            return Text(snapshot.error.toString(),
+                style: const TextStyle(color: Colors.red));
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
       ),
     );
   }
+
+  Future<List<Advert>> fetchAdverts() async {
+    final http.Response response =
+        await http.get(Uri.parse('http://localhost:3000/adverts'));
+    print("response.statusCode = ${response.statusCode}");
+    if (response.statusCode == 200) {
+      var lis = json.decode(response.body);
+      print("lis = $lis");
+      var lis2 = lis.map((advertData) => Advert.fromJson(advertData));
+      print("lis2 = $lis2");
+      var lis3 = lis2.toList();
+      print("lis3 = $lis3");
+      final List<Advert> adverts = lis2;
+      return adverts;
+    } else {
+      print("error");
+      throw Exception('Failed to load adverts');
+    }
+  }
 }
+
+//todo: borrar el provider y el AdvertsState
--- lib/states/adverts.dart
+++ lib/states/adverts.dart
@@ -3,64 +3,52 @@ import '../models/advert.dart';
 import '../models/user.dart';
 
 class AdvertsState extends ChangeNotifier {
-  List<Advert> adverts = [
-    Advert(
-        image: Image.network(
-          'https://cdni.pornpics.de/460/7/518/39935848/39935848_050_0044.jpg',
-        ),
-        user:
-            User(name: 'Rosa', desiredAge: 20, phoneNumber: '+57 301 345 6789'),
-        description: 'soy una nena bien caliente'),
-    Advert(
-        image: Image.network(
-          'https://cdni.pornpics.com/460/7/390/39177480/39177480_063_e13a.jpg',
-        ),
-        user:
-            User(name: 'Rosa', desiredAge: 20, phoneNumber: '+57 301 345 6789'),
-        description: 'soy una nena bien caliente'),
-    Advert(
-      image: Image.network(
-        'https://cdni.pornpics.com/460/1/364/44027804/44027804_007_39de.jpg',
-      ),
-      user: User(name: 'Rosa', desiredAge: 20, phoneNumber: '+57 301 345 6789'),
-      description: 'soy una nena bien caliente',
-    ),
-    Advert(
-      image: Image.network(
-        'https://cdni.pornpics.com/460/1/259/55374865/55374865_012_7c56.jpg',
-      ),
-      user: User(name: 'Rosa', desiredAge: 20, phoneNumber: '+57 301 345 6789'),
-      description: 'soy una nena bien caliente',
-    ),
-    Advert(
-      image: Image.network(
-        'https://cdni.pornpics.com/460/7/167/83446716/83446716_114_1bdd.jpg',
-      ),
-      user: User(name: 'Rosa', desiredAge: 20, phoneNumber: '+57 301 345 6789'),
-      description: 'soy una nena bien caliente',
-    ),
-    Advert(
-      image: Image.network(
-        'https://cdni.pornpics.com/460/7/548/11431620/11431620_054_75cb.jpg',
-      ),
-      user: User(name: 'Rosa', desiredAge: 20, phoneNumber: '+57 301 345 6789'),
-      description: 'soy una nena bien caliente',
-    ),
-    Advert(
-      image: Image.network(
-        'https://cdni.pornpics.com/460/7/146/99594170/99594170_034_4876.jpg',
-      ),
-      user: User(name: 'Rosa', desiredAge: 20, phoneNumber: '+57 301 345 6789'),
-      description: 'soy una nena bien caliente',
-    ),
-    Advert(
-      image: Image.network(
-        'https://plus.unsplash.com/premium_photo-1669868116752-acd4bc7bc6a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-      ),
-      user: User(name: 'Rosa', desiredAge: 20, phoneNumber: '+57 301 345 6789'),
-      description: 'soy una nena bien caliente',
-    ),
-  ];
+  List<Advert> adverts = [];
+  // List<Advert> adverts = [
+  //   Advert(
+  //       image: Image.network(
+  //         'https://cdni.pornpics.de/460/7/518/39935848/39935848_050_0044.jpg',
+  //       ),
+  //       user:
+  //           User(name: 'Rosa', desiredAge: 20, phoneNumber: '+57 301 345 6789'),
+  //       description: 'soy una nena bien caliente'),
+  //   Advert(
+  //       image: Image.network(
+  //         'https://cdni.pornpics.com/460/7/390/39177480/39177480_063_e13a.jpg',
+  //       ),
+  //       user:
+  //           User(name: 'Rosa', desiredAge: 20, phoneNumber: '+57 301 345 6789'),
+  //       description: 'soy una nena bien caliente'),
+  //   Advert(
+  //     image: Image.network(
+  //       'https://cdni.pornpics.com/460/1/364/44027804/44027804_007_39de.jpg',
+  //     ),
+  //     user: User(name: 'Rosa', desiredAge: 20, phoneNumber: '+57 301 345 6789'),
+  //     description: 'soy una nena bien caliente',
+  //   ),
+  //   Advert(
+  //     image: Image.network(
+  //       'https://cdni.pornpics.com/460/1/259/55374865/55374865_012_7c56.jpg',
+  //     ),
+  //     user: User(name: 'Rosa', desiredAge: 20, phoneNumber: '+57 301 345 6789'),
+  //     description: 'soy una nena bien caliente',
+  //   ),
+  //   Advert(
+  //     image: Image.network(
+  //       'https://cdni.pornpics.com/460/7/167/83446716/83446716_114_1bdd.jpg',
+  //     ),
+  //     user: User(name: 'Rosa', desiredAge: 20, phoneNumber: '+57 301 345 6789'),
+  //     description: 'soy una nena bien caliente',
+  //   ),
+  //   Advert(
+  //     image: Image.network(
+  //       'https://cdni.pornpics.com/460/7/548/11431620/11431620_054_75cb.jpg',
+  //     ),
+  //     user: User(name: 'Rosa', desiredAge: 20, phoneNumber: '+57 301 345 6789'),
+  //     description: 'soy una nena bien caliente',
+  //   ),
+  //   ];
+  // }
 
   void addAvert(Advert advert) {
     adverts.add(advert);
