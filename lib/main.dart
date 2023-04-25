25378885a1e70885d445a3098ddfc4e4a495e058
Christian <christian.garcerant@uac.edu.co>
2023-04-25 10:59:50 -0500

    Add the mime tag to the base64 in the advert model

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:swc_front/logic/cubits/authentication_cubit.dart';
-import 'package:swc_front/logic/cubits/user.dart';
 
 import 'presentation/router/app_router.dart';
 
