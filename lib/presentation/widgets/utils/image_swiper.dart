dbef3fae48a9b0a439d773bf8f55ec34191e6af6
Christian <christian.garcerant@uac.edu.co>
2023-05-10 14:01:13 -0500

    multi images details were fixed

--- lib/presentation/widgets/utils/image_swiper.dart
+++ lib/presentation/widgets/utils/image_swiper.dart
@@ -1,29 +1,69 @@
 import 'dart:typed_data';
-
-import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:dots_indicator/dots_indicator.dart';
+import 'package:flutter/material.dart';
 
-class ImageSlider extends StatelessWidget {
+class ImageSlider extends StatefulWidget {
   final List<Uint8List> images;
 
   ImageSlider({required this.images});
 
   @override
+  _ImageSliderState createState() => _ImageSliderState();
+}
+
+class _ImageSliderState extends State<ImageSlider> {
+  int _currentImageIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return CarouselSlider(
-      options: CarouselOptions(
-        autoPlay: false,
-        enlargeCenterPage: true,
-        aspectRatio: 9 / 16,
-        // viewportFraction: 1.0,
-      ),
-      items: images
-          .map((image) => Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height * 0.75,
-                child: Image.memory(image, fit: BoxFit.cover),
-              ))
-          .toList(),
+    return Column(
+      children: [
+        Expanded(
+          child: Stack(
+            children: [
+              CarouselSlider(
+                options: CarouselOptions(
+                  autoPlay: false,
+                  enlargeCenterPage: true,
+                  aspectRatio: 9 / 16,
+                  viewportFraction: 1,
+                  enlargeStrategy: CenterPageEnlargeStrategy.height,
+                  onPageChanged: (index, _) {
+                    setState(() {
+                      _currentImageIndex = index;
+                    });
+                  },
+                ),
+                items: widget.images
+                    .map(
+                      (image) => Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height * 0.75,
+                        child: Image.memory(image, fit: BoxFit.cover),
+                      ),
+                    )
+                    .toList(),
+              ),
+              Positioned(
+                bottom: 16.0,
+                left: 0.0,
+                right: 0.0,
+                child: DotsIndicator(
+                  dotsCount: widget.images.length,
+                  position: _currentImageIndex,
+                  decorator: DotsDecorator(
+                    activeColor: Colors.white,
+                    activeSize: const Size(7, 7),
+                    color: Colors.grey[400]!,
+                    size: const Size(4, 4),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
