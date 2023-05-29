afdcf1527d2011083bf3f06ae34dcbb070a8c79a
Christian <christian.garcerant@uac.edu.co>
2023-05-29 12:34:57 -0500

    bottom menu has been upgraded - isLogged functions implemented

--- lib/presentation/widgets/utils/bottom_sheet_menu.dart
+++ lib/presentation/widgets/utils/bottom_sheet_menu.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:swc_front/logic/cubits/authentication_cubit.dart';
 import 'package:swc_front/presentation/router/app_router.dart';
 import 'package:blurrycontainer/blurrycontainer.dart';
 
@@ -14,6 +16,7 @@ class _AppleMusicBottomSheetState extends State<AppleMusicBottomSheet> {
 
   @override
   Widget build(BuildContext context) {
+    bool isLogged = context.watch<AuthenticationCubit>().isLogged();
     return BlurryContainer(
       borderRadius: const BorderRadius.only(
         topLeft: Radius.circular(35),
@@ -24,6 +27,9 @@ class _AppleMusicBottomSheetState extends State<AppleMusicBottomSheet> {
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
         child: GNav(
+          mainAxisAlignment: !isLogged
+              ? MainAxisAlignment.spaceEvenly
+              : MainAxisAlignment.spaceBetween,
           selectedIndex: _selectedIndex,
           backgroundColor: const Color.fromRGBO(0, 0, 0, 0),
           color: Colors.grey,
@@ -40,30 +46,37 @@ class _AppleMusicBottomSheetState extends State<AppleMusicBottomSheet> {
                 Navigator.pushNamed(context, Routes.indexPage);
                 break;
               case 1:
-                Navigator.pushNamed(context, Routes.createAdvertPage);
+                !isLogged
+                    ? Navigator.pushNamed(context, Routes.loginPage)
+                    : Navigator.pushNamed(context, Routes.createAdvertPage);
                 break;
               case 2:
                 Navigator.pushNamed(context, Routes.loginPage);
                 break;
               case 3:
-                Navigator.pushNamed(context, Routes.editProfile);
+                !isLogged
+                    ? Navigator.pushNamed(context, Routes.loginPage)
+                    : Navigator.pushNamed(context, Routes.editProfile);
+
                 break;
             }
           },
-          tabs: const [
-            GButton(
+          tabs: [
+            const GButton(
               iconSize: 30,
               icon: CupertinoIcons.house_alt,
             ),
-            GButton(
-              iconSize: 30,
-              icon: CupertinoIcons.suit_heart,
-            ),
-            GButton(
-              iconSize: 30,
-              icon: CupertinoIcons.square_stack_3d_down_right,
-            ),
-            GButton(
+            if (isLogged)
+              const GButton(
+                iconSize: 30,
+                icon: CupertinoIcons.suit_heart,
+              ),
+            if (isLogged)
+              const GButton(
+                iconSize: 30,
+                icon: CupertinoIcons.square_stack_3d_down_right,
+              ),
+            const GButton(
               iconSize: 30,
               icon: CupertinoIcons.person_badge_plus,
             ),
